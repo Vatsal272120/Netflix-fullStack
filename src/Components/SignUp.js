90ad002705c5acfb3fcc6fd296bcdac5107c3cdd
Vatsal <vatsalpatel1073@gmail.com>
2021-02-13 18:15:34 +0530

    implementing the firebase authentication to the app

--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -1,16 +1,39 @@
+import { useRef } from "react";
+import { auth } from "../firebase";
 import "../Stylesheets/SignUp.css";
 
 const SignUp = () => {
+  const emailRef = useRef(null);
+  const passRef = useRef(null);
+
+  const register = (e) => {
+    e.preventDefault();
+
+    auth.createUserWithEmailAndPassword(
+      emailRef.current.value,
+      passRef.current.value
+    );
+  };
+
+  const signIn = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className='signUp'>
       <form action=''>
         <h1>Sign In</h1>
-        <input type='email' placeholder='Email' />
-        <input type='password' placeholder='Password' />
-        <button type='submit'>Sign In</button>
+        <input ref={emailRef} type='email' placeholder='Email' />
+        <input ref={passRef} type='password' placeholder='Password' />
+        <button type='submit' onClick={signIn}>
+          Sign In
+        </button>
         <h4>
           {" "}
-          <span className='signUp__grey'>New to Netflix? </span> Sign Up now.
+          <span className='signUp__grey'>New to Netflix? </span>{" "}
+          <span className='signUp__link' onClick={register}>
+            Sign Up now.
+          </span>
         </h4>
       </form>
     </div>
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyDndgPosScELEMf5XEE7bh9tXPfwkddMI4",
   authDomain: "netflix-eb88c.firebaseapp.com",
@@ -7,3 +9,10 @@ const firebaseConfig = {
   appId: "1:113522074370:web:1f1b0ec9e8ed7a66e7181b",
   measurementId: "G-J3EX7309X7",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { auth };
+export default db;

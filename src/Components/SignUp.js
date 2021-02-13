4962d0e646896e5c8f342c1292505e511b76aee7
Vatsal <vatsalpatel1073@gmail.com>
2021-02-13 11:00:41 +0530

    added the barebones structure to the sign Up page

--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -1,7 +1,17 @@
 import "../Stylesheets/SignUp.css";
 
 const SignUp = () => {
-  return <div className='signUp'></div>;
+  return (
+    <div className='signUp'>
+      <form action=''>
+        <h1>Sign In</h1>
+        <input type='email' placeholder='Email' />
+        <input type='password' placeholder='Password' />
+        <button type='submit'>Sign In</button>
+        <h4></h4>
+      </form>
+    </div>
+  );
 };
 
 export default SignUp;
